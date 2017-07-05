ef8e859455eb6f6c29483e5ceb1616e5e8b6ef0e
Dinh Hiep <hiepmanager@Dinhs-Mac.local>
2017-07-05 13:07:20 +0800

    Initial Commit

---
+++ ABC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ABC
+//
+//  Created by Dinh Duy Hiep on 7/5/17.
+//  Copyright © 2017 Dinh Duy Hiep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
